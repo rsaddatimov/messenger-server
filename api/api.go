30c0cc3a7439897fd6da848aaef6ea5639a2ab6e
rsaddatimov <rafa.saddatimov@gmail.com>
2020-08-22 02:16:49 +0300

    added initial source code and docker-compose.yml

---
+++ api/api.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+)
+
+type ServerAPI struct {
+	Conn *sql.DB
+}
+
+func(s *ServerAPI) AddUser(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "User was added!")
+}
+
+func(s *ServerAPI) AddChat(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func(s *ServerAPI) SendMessage(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func(s *ServerAPI) FetchChats(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func(s *ServerAPI) FetchChatsMessages(w http.ResponseWriter, r *http.Request) {
+
+}
